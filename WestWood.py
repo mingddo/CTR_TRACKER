8c9caeb8f045bddc6aa583a834d31aaac0a69a79
mingddo <dorosee97@gmail.com>
2021-09-06 13:46:00 +0900

    data save

--- WestWood.py
+++ WestWood.py
@@ -1,11 +1,13 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.select import Select
 from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import PatternFill
+from datetime import datetime
 
 
 def get_webdriver():
@@ -19,31 +21,53 @@ def get_webdriver():
     return driver
 
 
-def get_data(page):
+def get_data(page, container_no):
+    today = datetime.today().strftime("%Y%m%d")
     '''
     엑셀에 저장
 	* export.xlsx 와 동일하게 생성
     '''
-    nodes = page.select('#detail > tbody > tr')
     wb = Workbook()
     ws = wb.active
-    header = []
-    for head in page.select('#detail > thead > tr > th'):
-        header.append(head.text)
+    header = ['CTR NO', 'CY GATE IN','ON BOARD', 'PORT ETA', 'PORT ATA', 'RAIL ATA', 'CTR PICK UP', 'CTR RETURN', 'END']
+    # header = []
+    # for head in page.select('#HistoryData > div > table > thead > tr > th'):
+    #     header.append(head.text)
     ws.append(header)
-    
+    dataList = [0] * 9 #CTR 번호를 1열, 
+    dataList[0] = container_no
     # tbody 저장
-    for node in nodes:
-        tempData = node.select('td')
-        tempList = []
-        for d in tempData:
-            tempList.append(d.text.strip())
-        ws.append(tempList)
+    rows = page.select('#HistoryData > div > table > tbody > tr')
+    for i in range(len(rows)-1,-1,-1):
+        tempRow = rows[i].select('td')
+        event = tempRow[2].text.strip()
+        eventDate = tempRow[3].text.strip()
+        if event == 'Gate In Full':
+            dataList[1] = eventDate
+        elif 'Sailed' in event and 'POL' in event:
+            dataList[2] = eventDate
+        elif 'Arrived' in event and 'POD' in event:
+            if 'Estimated' in event:
+                dataList[3] = eventDate
+            else:
+                dataList[4] = eventDate
+        elif 'Rail' in event and 'Arrived' in event:
+            dataList[5] = eventDate
+        elif 'Gate Out' in event:
+            dataList[6] = eventDate
 
-    wb.save('One.xlsx')
 
 
-    
+    ws.append(dataList)
+    headerColor = PatternFill(start_color='c0c0c0', end_color='c0c0c0', fill_type='solid')
+    for col in range(1,10):
+        ws.column_dimensions[get_column_letter(col)].width = 20
+        ws.cell(1,col).fill = headerColor
+
+
+    wb.save(f'{today}-WestWood.xlsx')
+
+
 
 
 def controll_page(driver, container_no):
@@ -57,17 +81,14 @@ def controll_page(driver, container_no):
     target.click()
     target.clear()
     target.send_keys(container_no)
-    ###############여기까지 했다아
-    #################
-    driver.find_element_by_css_selector('#btnSearch').click()
-    time.sleep(2)
-    driver.execute_script(f"var a = document.querySelectorAll('td > a');for (var i=0; i<a.length;i++){{ if (a[i].innerText==='{container_no}') {{a[i].click();break;}}}}")
+
+    driver.find_element_by_css_selector('#ByContainer > div > div > div > button').click()
+    time.sleep(5)
+    # driver.execute_script(f"var a = document.querySelectorAll('td > a');for (var i=0; i<a.length;i++){{ if (a[i].innerText==='{container_no}') {{a[i].click();break;}}}}")
 
 
 if __name__ == '__main__':
     '''
-    Container : TCNU4277832, B/L : ONEYSELB95846900
-    Container : TEMU7180609, B/L : ONEYSELB65271501
     '''
     container_no = '#'
     url = 'https://www.wsl.com/Tracking/Public'
@@ -79,11 +100,11 @@ if __name__ == '__main__':
 
         page = bs(driver.page_source, 'lxml')
 
-        get_data(page)  # 데이터 추출후 프린트
+        get_data(page, container_no)  # 데이터 추출후 프린트
 
     finally:
-        # driver.quit()
-        pass
+        driver.quit()
+        # pass
 
     import sys
 
